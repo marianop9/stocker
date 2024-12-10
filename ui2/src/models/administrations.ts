0955bf0888b8d1428241eead926b7ee09fb13e50
Mariano Perez <perezm7401@gmail.com>
2024-12-10 11:01:05 -0300

    remove old ui

--- ui2/src/models/administrations.ts
+++
@@ -1,9 +0,0 @@
-export interface IDescribable {
-    id: string
-    name: string
-    description: string
-}
-
-export interface ICategory extends IDescribable {}
-
-export interface Provider extends IDescribable {}
